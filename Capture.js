06a02c41d8bf9d5cde9ebeb56129347d1dcca628
pandaxfly <pandaxfly@gmail.com>
2016-10-30 17:02:38 +0800

    Test JS OOP

---
+++ Capture.js
@@ -0,0 +1,12 @@
+// Capture.js is the modern web data collection framework for single page application
+//var a;
+//fetch('https://www.ns.sg').then(function(response){a=response});
+//a.headers.get('Content-Encoding');
+
+
+$('#test-header').on("click", function(event){
+	//console.log("pageY" + event.pageY + "clientY" + event.clientY + "screenY" + event.screenY);
+	console.log(event);
+	myEvent = event;
+	}
+);

e0b4948fcea3553682b883b2b1398e137ee33996
pandaxfly <pandaxfly@gmail.com>
2016-11-24 20:56:23 +0800

    Added basic function for Phase 1
    1. Enable capture attribute
    2. Capture click event

--- Capture.js
+++ Capture.js
@@ -4,23 +4,37 @@ window.capture = (function(){
 
 	// Constructor??
 	function Capture(){
-		var info = platform.parse(navigator.userAgent);
-		this.browserInfo = {
-			name: 			platform.name,
-			version: 		platform.version,
-			os: 			platform.os,
-			layout: 		platform.layout,
-			description: 	platform.description,
-			cookies: 		navigator.cookieEnabled,
-			screenSize: 	window.screen,
-		};
 	};
 
 	var capture = {
 		create: function(){
 			return new Capture();
+		},
+		browserInfo: function(){
+			var info = platform.parse(navigator.userAgent);
+			var browserInfo = {
+				name: 				platform.name,
+				version: 			platform.version,
+				os: 					platform.os,
+				layout: 			platform.layout,
+				description: 	platform.description,
+				cookies: 			navigator.cookieEnabled,
+				screenSize: 	window.screen,
+			};
+			return browserInfo;
+		},
+		list: function(){
+			var capturedItems = $("[capture]");
+			return capturedItems;
+		},
+		createEvent: function(){
+			$("[capture]").on( "click", function() { 
+		  	alert( $( this ).text() );
+			});
 		}
 	};
 
 	return capture;
 });
+
+var a = window.capture()
